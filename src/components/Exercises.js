d49402914f89d3309504d3acf2932f5c4be4453c
Saurabh Singh <42628047+singhSaurabh007@users.noreply.github.com>
2024-08-20 20:12:38 +0530

    Update Exercises.js

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -14,7 +14,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     let exercisesData = [];
 
     if (bodyPart === 'all') {
-      exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises?limit=10&offset=0', exerciseOptions);
     } else {
       exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}?limit=10&offset=0`, exerciseOptions);
     }
